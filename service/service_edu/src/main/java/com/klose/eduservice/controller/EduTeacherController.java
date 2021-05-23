e287411e3a8a6ec85602fa920c9b63ab2473f3b0
numbstan04 <2577971132@qq.com>
2021-05-23 23:26:18 +0800

    新增讲师列表获取接口，新增讲师逻辑删除接口，整合Swagger

--- service/service_edu/src/main/java/com/klose/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/klose/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,15 @@
 package com.klose.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.klose.eduservice.entity.EduTeacher;
+import com.klose.eduservice.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
  * @author testjava
  * @since 2021-05-23
  */
+@Api(description = "讲师管理")
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
 
+    @Autowired
+    private EduTeacherService eduTeacherService;
+
+    //查询讲师表所有数据
+    @ApiOperation(value = "所有讲师列表")
+    @GetMapping("findAll")
+    public List<EduTeacher> findAllTeacher() {
+        List<EduTeacher> list = eduTeacherService.list(null);
+        return list;
+    }
+
+    //逻辑删除讲师的方法
+    @ApiOperation(value = "逻辑删除讲师")
+    @DeleteMapping("{id}")
+    public boolean removeTeacher(@ApiParam(name = "id",value = "讲师ID",required = true) @PathVariable String id) {
+        boolean flag = eduTeacherService.removeById(id);
+        return flag;
+    }
 }
 
