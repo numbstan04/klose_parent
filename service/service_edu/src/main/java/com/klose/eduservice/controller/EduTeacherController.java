542be4d94e8c21b8fd40842ebe1d7cf6ce9adc84
numbstan04 <2577971132@qq.com>
2021-05-25 00:41:16 +0800

    统一结果返回，添加分页查询以及条件查询，添加添加讲师接口

--- service/service_edu/src/main/java/com/klose/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/klose/eduservice/controller/EduTeacherController.java
@@ -1,14 +1,24 @@
 package com.klose.eduservice.controller;
 
 
+
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.klose.commonutils.R;
 import com.klose.eduservice.entity.EduTeacher;
+import com.klose.eduservice.entity.vo.TeacherQuery;
 import com.klose.eduservice.service.EduTeacherService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.management.Query;
+import javax.swing.text.Keymap;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -30,17 +40,99 @@ public class EduTeacherController {
     //查询讲师表所有数据
     @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher() {
+    public R findAllTeacher() {
         List<EduTeacher> list = eduTeacherService.list(null);
-        return list;
+        return R.ok().data("items", list);
     }
 
     //逻辑删除讲师的方法
     @ApiOperation(value = "逻辑删除讲师")
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@ApiParam(name = "id",value = "讲师ID",required = true) @PathVariable String id) {
+    public R removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id) {
         boolean flag = eduTeacherService.removeById(id);
-        return flag;
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
+    //分页查询讲师
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,//当前页
+                             @PathVariable long limit) { //每页记录数
+        //创建page对象
+        Page<EduTeacher> page = new Page<>(current, limit);
+        eduTeacherService.page(page, null);
+
+        //记录总数
+        long total = page.getTotal();
+
+        //数据list集合
+        List<EduTeacher> records = page.getRecords();
+
+        //方式一
+        HashMap hashMap = new HashMap();
+        hashMap.put("total", total);
+        hashMap.put("row", records);
+        return R.ok().data(hashMap);
+
+        //方式二
+        //return R.ok().data("total", total).data("row", records);
+    }
+
+    //条件查询带分页的方法
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable long current,
+                                  @PathVariable long limit,
+                                  //参数值可以为空
+                                  @RequestBody(required = false) TeacherQuery teacherQuery) {
+        //创建page对象
+        Page<EduTeacher> page = new Page<>(current, limit);
+        //条件构建
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+
+        //多条件查询
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        //判断条件值是否为空，如果不为空则拼接
+        if (!StringUtils.isEmpty(name)) {
+            wrapper.like("name", name);
+        }
+        if (!StringUtils.isEmpty(level)) {
+            wrapper.eq("level", level);
+        }
+        if (!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create", begin);
+        }
+        if (!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create", end);
+        }
+
+        //调用方法实现条件查询分页
+        eduTeacherService.page(page, wrapper);
+
+        //记录总数
+        long total = page.getTotal();
+
+        //数据list集合
+        List<EduTeacher> records = page.getRecords();
+
+        return R.ok().data("total", total).data("row", records);
+
+    }
+
+    //添加讲师接口
+    @PostMapping("addTeacher")
+    public R addTeacher(@RequestBody EduTeacher eduTeacher) {
+        boolean save = eduTeacherService.save(eduTeacher);
+        if (save) {
+            return R.ok();
+        }else{
+            return R.error();
+        }
     }
 }
 
